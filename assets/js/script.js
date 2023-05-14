5e9066110d6c48e3861d4bf9446d7c437708beaa
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-14 13:11:41 -0400

    add js file and write pseudocode start and add more content to html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// pseudocode coding quiz
+// When I click the start button, then a timer starts and I am presented with a question
+
+// when I answer a question I am presented with another question
+
+// When I answer the question incorrectly then time is subtracted from the clock
+
+// When all questions are answered pr the timer reaches 0 then the game is over
+
+// when the game is over then I can save my initials and score
