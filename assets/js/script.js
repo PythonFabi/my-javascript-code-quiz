6923e22bc9da13b90fe155058f756709a182034e
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-14 20:21:24 -0400

    change html for start page, add css for quiz and header, and add js start function, timer, first and second question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,82 @@
 // pseudocode coding quiz
+var startButton = document.querySelector(".start-button");
+var timeEl = document.querySelector(".counter");
+var questionSection = document.querySelector(".question-section");
+var firstQuestionAnswers = ['1. strings', '2. booleans', '3. alerts', '4. numbers'];
+var secondQuestionAnswers = ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets'];
+var timer;
+var timerCount;
+
 // When I click the start button, then a timer starts and I am presented with a question
 /* If (start button is clicked) {
     timer starts
     first question } */
+    function startGame() {
+        timerCount = 75;
+        setTimer();
+        firstQuestion();
+    };
+
+// Timer decrements seond by second until it reaches 0 and calls the game over function
+   function setTimer() {
+       
+       timer = setInterval(function() {  
+            timerCount--;
+            timeEl.textContent = timerCount;
+
+            if(timerCount === 0) {
+                clearInterval(timer);
+                gameOver();
+
+            }
+        }, 1000);
+    };
+
+// First question and buttons appear after clicking the start quiz button
+    function firstQuestion() {
+        questionSection.innerHTML = "";
+        var firstQuestion = document.createElement("p");
+        firstQuestion.textContent = "Commonly used data types do NOT include:"
+        firstQuestion.setAttribute("class", "questions");
+        questionSection.appendChild(firstQuestion);
+
+        // loops through the array of firstQuestion answers
+        for (let i = 0; i < firstQuestionAnswers.length; i++) {
+            var answers = document.createElement("button");
+            answers.textContent = firstQuestionAnswers[i];
+            answers.setAttribute("class", "question-buttons");
+
+            answers.addEventListener('click', function (){
+                secondQuestion();
+            });
+
+            questionSection.appendChild(answers);
+        };
+
+        
+    
+    
+    };
+
+
+    function secondQuestion(){
+        questionSection.innerHTML = "";
+        var secondQuestion = document.createElement("p");
+        secondQuestion.textContent = "The condition in an if/else statement is endlosed with________."
+        secondQuestion.setAttribute("class", "questions");
+        questionSection.appendChild(secondQuestion);
+
+        for (let i = 0; i < secondQuestionAnswers.length; i++) {
+            var answers = document.createElement("button");
+            answers.textContent = secondQuestionAnswers[i];
+            answers.setAttribute("class", "question-buttons");
+
+            questionSection.appendChild(answers);
+        };
+    };
+
+
+    startButton.addEventListener('click', startGame);
 
 // when I answer a question I am presented with another question
 /*If question answered {
